f971728ec58cf50fde14808c9de39f07a7551752
Nate Xiao <nathanielxiao@outlook.com>
2023-09-18 17:06:00 -0600

    修改了 跳跃游戏IV注释

--- 算法/基础算法模版/动态规划/跳跃游戏IV.py
+++ 算法/基础算法模版/动态规划/跳跃游戏IV.py
@@ -51,7 +51,11 @@ class Solution:
             同时满足以下三个条件才能是视为此距离可以被到达
             1. 到i的距离已经达到minJump的要求
             2. s[i]为0
-            3. 到i的距离没有超过maxJump， 或者i>maxJump同时sum(dp[i-maxJump:i-minJump+1])>0
+            3. 到i的距离没有超过maxJump， 或着i>maxJump同时 i-maxJump 到 i-minJump 直接任何一位可以到达（代表着当前i的位置可以被到达）
+            
+            dp[i-minJump]-dp[i-maxJump-1] >= 1
+            因为是前缀和，所以dp[i-minJump]（代表的是i-minJump的位置是否可以被到达）-dp[i-maxJump-1]（代表的是i-maxJump-1的位置是否可以被到达）的区间合>=1
+            就代表i-maxJump-1到i-minJump之间有可以被到达的位置。
             '''
             dpVal = s[i] == "0" and i >= minJump and (i<= maxJump or dp[i-minJump]-dp[i-maxJump-1] >= 1)
             # 前缀和的操作
