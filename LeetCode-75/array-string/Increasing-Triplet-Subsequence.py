95147b79004affa70d7081a23161005bc28214ee
Nate Xiao <nathanielxiao@outlook.com>
2023-09-21 11:34:12 -0600

    Increasing Triplet Subsequence

---
+++ LeetCode-75/array-string/Increasing-Triplet-Subsequence.py
@@ -0,0 +1,19 @@
+'''
+LeetCode: 334 https://leetcode.com/problems/increasing-triplet-subsequence/description/?envType=study-plan-v2&envId=leetcode-75
+'''
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        '''
+        any three elements being incremental
+        greedy
+        '''
+        first = second = float('inf')
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        return False
