f6fa832a8348cb00fb2a5fc516e8cd297b905f1a
Nate Xiao <nathanielxiao@outlook.com>
2023-09-25 08:21:19 -0600

    added 1679

---
+++ LeetCode-75/two-pointers/Max-Number-of-K-Sum-Pairs.py
@@ -0,0 +1,19 @@
+'''
+leetcode 1679 https://leetcode.com/problems/max-number-of-k-sum-pairs/
+'''
+
+class Solution:
+    def maxOperations(self, nums: list[int], k: int) -> int:
+        nums.sort()
+        l, r = 0, len(nums)-1
+        ans = 0
+        while l < r:
+            if nums[l] + nums[r] == k:
+                ans += 1
+                r -= 1
+                l += 1
+            elif nums[l] + nums[r] > k:
+                r -= 1
+            else:
+                l += 1
+        return ans
